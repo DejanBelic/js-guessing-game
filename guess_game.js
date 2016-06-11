cae9fe4b4ae9d893edbacb37d8841d7916661656
Dejan Belic <dejan.belic8@gmail.com>
2016-06-11 14:27:58 +0200

    Update guess_game.js

--- guess_game.js
+++ guess_game.js
@@ -1,37 +1,37 @@
 // Global Variables
 var target;
-var guess_input_text;
-var guess_input;
+var guessInputText;
+var guessInput;
 var finished = false;
 var guesses = 0;
 
-// game function
-function start_game() {
-	// random number from 1 - 100
-	var random_number = Math.random() * 100;
-	var random_number_integer = Math.floor(random_number);
-	target = random_number_integer + 1;
-				// user input 
+// Game Function
+function startGame() {
+	// Random Number From 1 - 100
+	var randomNumber = Math.random() * 100;
+	var randomNumberInteger = Math.floor(randomNumber);
+	target = randomNumberInteger + 1;
+				// User Input 
 	while (!finished) {
-		guess_input_text = prompt("I am thinking of a number between 1  to 100 . \n" + "Can you guess number that im thinking of?");
-		guess_input = parseInt(guess_input_text);
+		guessInputText = prompt("I am thinking of a number between 1  to 100 . \n" + "Can you guess number that im thinking of?");
+		guessInput = parseInt(guessInputText);
 		guesses += 1;
-		finished = check_guess();
-	}			// checking inputs
-	function check_guess() {
-		if (isNaN(guess_input)) {
+		finished = checkGuess();
+	}			// Checking Inputs
+	function checkGuess() {
+		if (isNaN(guessInput)) {
 			alert("Please enter a number");
 			return false;
 		}
-		if ((guess_input < 1) || (guess_input > 100)) {
+		if ((guessInput < 1) || (guessInput > 100)) {
 			alert("Please enter number between 1 and 100");
 			return false;
 		}
-		if (guess_input > target) {
+		if (guessInput > target) {
 			alert("Your number is too large!");
 			return false;
 		}
-		if (guess_input < target) {
+		if (guessInput < target) {
 			alert("Your number is too small!");
 			return false;
 		}
